20dfa07ae457c0112df4f6761312a205bead0016
Akindele Michael <akindelemichael65@gmail.com>
2024-02-18 16:17:04 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/mikeinvents/yoruba_clarity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mikeinvents.yoruba_clarity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/yc_app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class YCAppBar extends StatelessWidget  implements PreferredSizeWidget {
+  const YCAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const App();
+  }
+}
